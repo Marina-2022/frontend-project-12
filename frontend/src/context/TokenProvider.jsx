c75023d111e2c9289efe99f1257b01938f5ba92a
Marina Garipova <naumarina@yandex.ru>
2024-07-15 21:14:08 +0800

    feat: add TokenContext and TokenProvider

---
+++ frontend/src/context/TokenProvider.jsx
@@ -0,0 +1,31 @@
+import React, {
+  useState, useEffect, useMemo, useCallback,
+} from 'react';
+import TokenContext from './AuthContext.js';
+
+const TokenProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem('token') === null ? '' : localStorage.getItem('token'));
+  const [userName, setUsername] = useState(localStorage.getItem('username') === null ? '' : localStorage.getItem('username'));
+
+  const saveToken = useCallback((newToken) => setToken(newToken), []);
+
+  const saveUsername = useCallback((newUserName) => setUsername(newUserName), []);
+
+  useEffect(() => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('username', userName);
+  }, [token, userName]);
+
+  const contextAuthValue = useMemo(() => (
+    {
+      token, userName, saveToken, saveUsername,
+    }), [token, userName, saveToken, saveUsername]);
+
+  return (
+    <TokenContext.Provider value={contextAuthValue}>
+      {children}
+    </TokenContext.Provider>
+  );
+};
+
+export default TokenProvider;
