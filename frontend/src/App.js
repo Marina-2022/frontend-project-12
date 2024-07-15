7a500d2f69373119152c492976c890d15ae8f659
Marina Garipova <naumarina@yandex.ru>
2024-07-15 20:57:17 +0800

    feat: add new Route Chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
-// import logo from './logo.svg';
+/* eslint-disable */
+import React, { useContext } from 'react';
 import './App.css';
 import {
   BrowserRouter,
   Route,
   Routes,
 } from 'react-router-dom';
+// import TokenContext from './context/AuthContext.js';
 import Login from './components/Login';
 import NotFound from './components/NotFound';
+import Chat from './components/Chat';
+
+// const { token } = useContext(TokenContext);
 
 const App = () => (
   <div className="d-flex flex-column h-100">
     {/* <Login /> */}
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Chat />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
