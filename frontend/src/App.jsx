3a11752af4561750cbf5a4538e388afaa9317a77
Marina Garipova <naumarina@yandex.ru>
2024-07-16 12:34:08 +0800

    changed ext App.jsx and add PrivateRoute

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import React, { useContext } from 'react';
+import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+// import TokenContext from './context/AuthContext.js';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
+import Chat from './components/Chat';
+import PrivateRoute from './components/PrivateRoute';
+
+// const { token } = useContext(TokenContext);
+
+const App = () => (
+  <div className="d-flex flex-column h-100">
+    {/* <Login /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PrivateRoute><Chat /></PrivateRoute>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+);
+
+export default App;
