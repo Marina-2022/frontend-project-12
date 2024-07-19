aaa20963c9acc3470fdbb2dd71687d903ee96e91
Marina Garipova <naumarina@yandex.ru>
2024-07-19 19:57:54 +0800

    add dir locales and ru.js index.js

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,14 @@
+const translation = {
+  translation: {
+    header: {
+      hexlet: 'Hexlet Chat',
+      exit: 'Выйти',
+    },
+    chat: {
+      channels: 'Каналы',
+      plus: '+',
+    },
+  },
+};
+
+export default translation;
