64a7af8ac0b31c38b625af51700f0e7ccc850327
Marina Garipova <naumarina@yandex.ru>
2024-07-19 19:39:40 +0800

    feat: add Button logOut

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import {
+  Navbar, Container, Nav, Button,
+} from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import useAuth from '../hooks/useAuth';
 
-const Header = () => (
-  <Navbar expand="lg" bg="white" variant="light" className="shadow-sm">
-    <Container>
-      <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
-      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto" />
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+const Header = () => {
+  const { t } = useTranslation();
+  const auth = useAuth();
+
+  return (
+    <Navbar expand="lg" bg="white" variant="light" className="shadow-sm">
+      <Container>
+        <Navbar.Brand href="/">
+          {t('header.hexlet')}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {auth.token ? (
+              <Button variant="primary" onClick={auth.logOut}>
+                {t('header.exit')}
+              </Button>
+            ) : (
+              ''
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
