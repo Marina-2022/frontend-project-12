7959be823c8b7ab5cd6ce681e35b4e03fd3f6e1b
Marina Garipova <naumarina@yandex.ru>
2024-07-19 19:45:54 +0800

    this file all commented

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,10 +1,10 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+// import { Navigate } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
 
-const PrivateRoute = ({ children }) => {
-  const token = useSelector((state) => state.auth.token);
+// const PrivateRoute = ({ children }) => {
+//   const token = useSelector((state) => state.auth.token);
 
-  return token ? children : <Navigate to="/login" />;
-};
+//   return token ? children : <Navigate to="/login" />;
+// };
 
-export default PrivateRoute;
+// export default PrivateRoute;
