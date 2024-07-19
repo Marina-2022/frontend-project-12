aec3b937c6ee102b286b87dfcca2b6e2643be574
Marina Garipova <naumarina@yandex.ru>
2024-07-19 19:51:00 +0800

    currentChannel and middleware

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../slices/authSlice';
+import currentChannelReducer from '../slices/currentChannelSlice';
+import channelsApi from '../api/channelsApi.js';
 
 export default configureStore({
   reducer: {
+    [channelsApi.reducerPath]: channelsApi.reducer,
     auth: authReducer,
+    currentChannel: currentChannelReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
+    channelsApi.middleware,
+  ),
 });
