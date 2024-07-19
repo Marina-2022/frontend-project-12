6adc8171d4bdc966c5480a6c772e0d5c9229a976
Marina Garipova <naumarina@yandex.ru>
2024-07-19 19:53:36 +0800

    add folder dir helpers and prepareHeaders

---
+++ frontend/src/helpers/prepareHeaders.js
@@ -0,0 +1,16 @@
+const prepareHeaders = (headers) => {
+  const token = localStorage.getItem('token');
+  // console.log('Token from localStorage:', token);
+  if (token) {
+    headers.set('Authorization', `Bearer ${token}`);
+    // console.log('Headers with token:', headers);
+  }
+  return headers;
+};
+
+export default prepareHeaders;
+
+// (headers) => {
+//   headers.set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+//   console.log('localStorage', localStorage.getItem('token'));
+//   return headers;
