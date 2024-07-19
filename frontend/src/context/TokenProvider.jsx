f9536427ff68672e9c73f93dcb7d96a7ecbcceec
Marina Garipova <naumarina@yandex.ru>
2024-07-19 19:47:59 +0800

    this file all commented

--- frontend/src/context/TokenProvider.jsx
+++ frontend/src/context/TokenProvider.jsx
@@ -1,31 +1,32 @@
-import React, {
-  useState, useEffect, useMemo, useCallback,
-} from 'react';
-import TokenContext from './AuthContext.jsx';
+/* eslint-disable */
+// import React, {
+//   useState, useEffect, useMemo, useCallback,
+// } from 'react';
+// import TokenContext from './AuthContext.jsx';
 
-const TokenProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem('token') === null ? '' : localStorage.getItem('token'));
-  const [userName, setUsername] = useState(localStorage.getItem('username') === null ? '' : localStorage.getItem('username'));
+// const TokenProvider = ({ children }) => {
+//   const [token, setToken] = useState(localStorage.getItem('token') === null ? '' : localStorage.getItem('token'));
+//   const [userName, setUsername] = useState(localStorage.getItem('username') === null ? '' : localStorage.getItem('username'));
 
-  const saveToken = useCallback((newToken) => setToken(newToken), []);
+//   const saveToken = useCallback((newToken) => setToken(newToken), []);
 
-  const saveUsername = useCallback((newUserName) => setUsername(newUserName), []);
+//   const saveUsername = useCallback((newUserName) => setUsername(newUserName), []);
 
-  useEffect(() => {
-    localStorage.setItem('token', token);
-    localStorage.setItem('username', userName);
-  }, [token, userName]);
+//   useEffect(() => {
+//     localStorage.setItem('token', token);
+//     localStorage.setItem('username', userName);
+//   }, [token, userName]);
 
-  const contextAuthValue = useMemo(() => (
-    {
-      token, userName, saveToken, saveUsername,
-    }), [token, userName, saveToken, saveUsername]);
+//   const contextAuthValue = useMemo(() => (
+//     {
+//       token, userName, saveToken, saveUsername,
+//     }), [token, userName, saveToken, saveUsername]);
 
-  return (
-    <TokenContext.Provider value={contextAuthValue}>
-      {children}
-    </TokenContext.Provider>
-  );
-};
+//   return (
+//     <TokenContext.Provider value={contextAuthValue}>
+//       {children}
+//     </TokenContext.Provider>
+//   );
+// };
 
-export default TokenProvider;
+// export default TokenProvider;
