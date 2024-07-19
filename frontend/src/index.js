3b277d16bcd247a2090050b4734ee60cff2fa3ff
Marina Garipova <naumarina@yandex.ru>
2024-07-19 19:49:05 +0800

    feat: add I18nextProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n.js';
 import App from './App';
 import store from './store/index.js';
-import TokenProvider from './context/TokenProvider.jsx';
+// import TokenProvider from './context/TokenProvider.jsx';
 
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('chat'));
 root.render(
   <React.StrictMode>
-    <TokenProvider>
+    <I18nextProvider i18n={i18n}>
       <Provider store={store}>
         <App />
       </Provider>
-    </TokenProvider>
+    </I18nextProvider>
   </React.StrictMode>,
 );
 
