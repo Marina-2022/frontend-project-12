abb16dd42b17437e5403c39211084710a24873e7
Marina Garipova <naumarina@yandex.ru>
2024-07-19 19:37:59 +0800

    commented

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,32 +1,44 @@
+/* eslint-disable */
+// import axios from 'axios';
 import React from 'react';
-import Header from './Header';
+// import { useSelector } from 'react-redux';
+// import useAuth from '../hooks/useAuth';
+import Channels from './Channels';
+import { setCurrentChannel } from '../slices/currentChannelSlice';
 
-const Chat = () => (
-  <div>
-    <Header />
-    <div className="container h-100 my-4 overflow-hidden rounded shadow">
-      <div className="row h-100 bg-white flex-md-row">
-        <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-          <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-            <b>Каналы</b>
-            <button type="button" className="p-0 text-primary btn btn-group-vertical">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                width="20"
-                height="20"
-                fill="currentColor"
-              >
-                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-              </svg>
-              {/* <span className="visually-hidden"></span> */}
-            </button>
-          </div>
+const Chat = () => {
+  // const auth = useAuth();
+  // const state = useSelector((state) => state);
+
+  // console.log('Redux State:', state);
+
+
+  // useEffect(() => {
+  //   const getChannels = async () => {
+  //     await axios.get('/api/v1/channels', {
+  //       headers: {
+  //         Authorization: `Bearer ${auth.token}`,
+  //       },
+  //     }).then((response) => {
+  //       console.log(response.data); // =>[{ id: '1', name: 'general', removable: false }, ...]
+  //     })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   };
+  //   getChannels();
+  // }, []);
+
+  return (
+    <>
+      {/* <Header /> */}
+      <div className="container h-100 my-4 overflow-hidden rounded shadow">
+        <div className="row h-100 bg-white flex-md-row">
+          <Channels />
         </div>
       </div>
-    </div>
-  </div>
-);
+    </>
+  );
+};
 
 export default Chat;
