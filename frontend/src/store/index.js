5ca906f99269f1fa6d93256591b564242638f0b7
Marina Garipova <naumarina@yandex.ru>
2024-07-25 19:06:32 +0800

    add import modalSliceReducer and messagesApi

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../slices/authSlice';
 import currentChannelReducer from '../slices/currentChannelSlice';
+import modalSliceReducer from '../slices/modalSlice.js';
 import channelsApi from '../api/channelsApi.js';
+import messagesApi from '../api/messagesApi.js';
 
 export default configureStore({
   reducer: {
     [channelsApi.reducerPath]: channelsApi.reducer,
+    [messagesApi.reducerPath]: messagesApi.reducer,
     auth: authReducer,
     currentChannel: currentChannelReducer,
+    modalChannel: modalSliceReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
     channelsApi.middleware,
+    messagesApi.middleware,
   ),
 });
