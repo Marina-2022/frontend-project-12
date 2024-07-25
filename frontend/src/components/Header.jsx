45a1ff70e4a5a99dd0d5d0a93f45cdd906332e33
Marina Garipova <naumarina@yandex.ru>
2024-07-25 19:00:57 +0800

    feat: add className=text-decoration-none for Link

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Navbar, Container, Nav, Button,
 } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import useAuth from '../hooks/useAuth';
 
@@ -9,11 +10,15 @@ const Header = () => {
   const { t } = useTranslation();
   const auth = useAuth();
 
+  // console.log('localStorage', localStorage.getItem('token'));
+
   return (
     <Navbar expand="lg" bg="white" variant="light" className="shadow-sm">
       <Container>
-        <Navbar.Brand href="/">
-          {t('header.hexlet')}
+        <Navbar.Brand>
+          <Link className="text-decoration-none text-black" to="/">
+            {t('header.hexlet')}
+          </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
