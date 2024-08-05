9d9be83463622352e7e1c9434412bf46ca0d302e
Marina Garipova <naumarina@yandex.ru>
2024-08-05 22:02:01 +0800

    add RemoveChannel.jsx

---
+++ frontend/src/components/modals/RemoveChannel.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import Modal from 'react-bootstrap/Modal';
+import { toast } from 'react-toastify';
+import { useRemoveChannelMutation } from '../../api/channelsApi';
+import { setCurrentChannel } from '../../slices/currentChannelSlice';
+
+const RemoveChannel = (props) => {
+  const { showModal, handleClose, modalChannelId } = props;
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const [removeChannel] = useRemoveChannelMutation();
+
+  const currentChannelId = useSelector((state) => state.currentChannel.id);
+
+  const defaultChannel = { id: '1', name: 'general', removable: false };
+
+  const handleRemoveChannel = async (id) => {
+    try {
+      await removeChannel(id).unwrap();
+      handleClose();
+      if (id === currentChannelId) {
+        dispatch(setCurrentChannel(defaultChannel));
+      }
+      toast.success(t('toast.channelDeleted'));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Modal show={showModal === 'removing'} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>{t('chat.removeChannel')}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p className="lead">{t('chat.removeChannelText')}</p>
+        <div className="d-flex justify-content-end">
+          <button
+            type="button"
+            className="me-2 btn btn-secondary"
+            onClick={handleClose}
+          >
+            {t('chat.cancel')}
+          </button>
+          <button
+            type="submit"
+            className="btn btn-danger"
+            onClick={() => handleRemoveChannel(modalChannelId)}
+          >
+            {t('chat.delete')}
+          </button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default RemoveChannel;
