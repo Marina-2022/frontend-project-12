aee9727fed12471515020b27cc6e611d1f1f32b9
Marina Garipova <naumarina@yandex.ru>
2024-08-08 21:59:12 +0800

    add useTranslation

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import imageNotFound from '../images/errorPage.png';
 // import Header from './Header';
 
-const NotFound = () => (
-  <>
-    {/* <Header /> */}
+const NotFound = () => {
+  const { t } = useTranslation();
+
+  return (
     <div className="text-center">
       <img
         className="img-fluid w-50"
         src={imageNotFound}
-        alt="Страница не найдена"
+        alt={t('notFound.pageNotFound')}
       />
-      <h1 className="h4 text-muted">Страница не найдена</h1>
+      <h1 className="h4 text-muted">{t('notFound.pageNotFound')}</h1>
       <p className="text-muted">
-        Но вы можете перейти
+        {t('notFound.youCanGo')}
         {' '}
-        <a href="/">на главную страницу</a>
+        <a href="/">{t('notFound.toTheHomepage')}</a>
       </p>
     </div>
-  </>
-);
+  );
+};
 
 export default NotFound;
