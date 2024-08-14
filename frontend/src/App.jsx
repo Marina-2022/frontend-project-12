c796c46da20bed05d1595a3a874dcb33dd49ffc9
Marina Garipova <naumarina@yandex.ru>
2024-08-14 21:18:30 +0800

    refactor: authorization moved to authSlice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 // /* eslint-disable */
-import React, {
-  useCallback, useEffect, useState, useMemo,
-} from 'react';
+import React from 'react';
+// import React, {
+//   useCallback, useEffect, useState, useMemo,
+// } from 'react';
 import './App.css';
 import {
   BrowserRouter,
@@ -14,13 +15,15 @@ import {
   Provider as ProviderRollbar,
   ErrorBoundary,
 } from '@rollbar/react';
-import AuthContext from './context/AuthContext.jsx';
+import { useSelector } from 'react-redux';
+// import AuthContext from './context/AuthContext.jsx';
 import Login from './components/Login';
 import NotFound from './components/NotFound';
 import Chat from './components/Chat';
 import Signup from './components/Signup';
-import useAuth from './hooks/useAuth.js';
+// import useAuth from './hooks/useAuth.js';
 import Header from './components/Header.jsx';
+import { selectLoggedIn } from './slices/authSlice';
 
 const rollbarConfig = {
   accessToken: process.env.REACT_APP_TOKEN_ACCESS,
@@ -29,67 +32,96 @@ const rollbarConfig = {
   invironment: 'production',
 };
 
-const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem('token') || '');
-  const [userName, setUsername] = useState(localStorage.getItem('username') || '');
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    if (token) {
-      localStorage.setItem('token', token);
-      localStorage.setItem('username', userName);
-    } else {
-      localStorage.removeItem('token');
-      localStorage.removeItem('username');
-    }
-  }, [token, userName]);
-
-  const logIn = useCallback(() => setLoggedIn(true), []);
-  const logOut = useCallback(() => {
-    setToken('');
-    setUsername('');
-    setLoggedIn(false);
-    localStorage.removeItem('token');
-    localStorage.removeItem('username');
-  }, []);
-
-  const contextAuthValue = useMemo(() => (
-    {
-      token, userName, loggedIn, logIn, logOut, setToken, setUsername,
-    }), [token, userName, loggedIn, logIn, logOut, setToken, setUsername]);
-
-  return (
-    <AuthContext.Provider value={contextAuthValue}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
 const PrivateRoute = ({ children }) => {
-  const auth = useAuth();
+  const loggedIn = useSelector(selectLoggedIn);
 
-  return auth.loggedIn ? children : <Navigate to="/login" />;
+  return loggedIn ? children : <Navigate to="/login" />;
 };
 
 const App = () => (
   <div className="d-flex flex-column h-100">
     <ProviderRollbar config={rollbarConfig}>
       <ErrorBoundary>
-        <AuthProvider>
-          <BrowserRouter>
-            <Header />
-            <Routes>
-              <Route path="/" element={<PrivateRoute><Chat /></PrivateRoute>} />
-              <Route path="/login" element={<Login />} />
-              <Route path="*" element={<NotFound />} />
-              <Route path="/signup" element={<Signup />} />
-            </Routes>
-            <ToastContainer />
-          </BrowserRouter>
-        </AuthProvider>
+        {/* <AuthProvider> */}
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<PrivateRoute><Chat /></PrivateRoute>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+          <ToastContainer />
+        </BrowserRouter>
+        {/* </AuthProvider> */}
       </ErrorBoundary>
     </ProviderRollbar>
   </div>
 );
 
 export default App;
+
+// const AuthProvider = ({ children }) => {
+//   const [token, setToken] = useState(localStorage.getItem('token') || '');
+//   const [userName, setUsername] = useState(localStorage.getItem('username') || '');
+//   const [loggedIn, setLoggedIn] = useState(false);
+
+//   useEffect(() => {
+//     if (token) {
+//       localStorage.setItem('token', token);
+//       localStorage.setItem('username', userName);
+//     } else {
+//       localStorage.removeItem('token');
+//       localStorage.removeItem('username');
+//     }
+//   }, [token, userName]);
+
+//   const logIn = useCallback(() => setLoggedIn(true), []);
+//   const logOut = useCallback(() => {
+//     setToken('');
+//     setUsername('');
+//     setLoggedIn(false);
+//     localStorage.removeItem('token');
+//     localStorage.removeItem('username');
+//   }, []);
+
+//   const contextAuthValue = useMemo(() => (
+//     {
+//       token, userName, loggedIn, logIn, logOut, setToken, setUsername,
+//     }), [token, userName, loggedIn, logIn, logOut, setToken, setUsername]);
+
+//   return (
+//     <AuthContext.Provider value={contextAuthValue}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// const PrivateRoute = ({ children }) => {
+//   const auth = useAuth();
+
+//   return auth.loggedIn ? children : <Navigate to="/login" />;
+// };
+
+// const App = () => (
+//   <div className="d-flex flex-column h-100">
+//     <ProviderRollbar config={rollbarConfig}>
+//       <ErrorBoundary>
+//         <AuthProvider>
+//           <BrowserRouter>
+//             <Header />
+//             <Routes>
+//               <Route path="/" element={<PrivateRoute><Chat /></PrivateRoute>} />
+//               <Route path="/login" element={<Login />} />
+//               <Route path="*" element={<NotFound />} />
+//               <Route path="/signup" element={<Signup />} />
+//             </Routes>
+//             <ToastContainer />
+//           </BrowserRouter>
+//         </AuthProvider>
+//       </ErrorBoundary>
+//     </ProviderRollbar>
+//   </div>
+// );
+
+// export default App;
