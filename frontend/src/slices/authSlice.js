206c56923e21969bb2ee5563743f67efd9fd4f45
Marina Garipova <naumarina@yandex.ru>
2024-08-14 23:46:26 +0800

    refactor loggedIn

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -1,30 +1,35 @@
-/* eslint-disable no-param-reassign */
+// /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   token: localStorage.getItem('token') ? localStorage.getItem('token') : null,
   userName: localStorage.getItem('username') ? localStorage.getItem('username') : '',
-  loggedIn: !!localStorage.getItem('token'),
+  // loggedIn: !!localStorage.getItem('token'),
+  // loggedIn: false,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setToken: (state, action) => {
-      state.token = action.payload;
-      state.loggedIn = !!action.payload; // Устанавливаем статус в зависимости от наличия токена
-    },
+    setToken: (state, action) => ({
+      ...state,
+      token: action.payload,
+      // state.token = action.payload;
+      // state.loggedIn = !!action.payload;
+    }),
 
-    setUserName(state, action) {
-      state.userName = action.payload;
-    },
+    setUserName: (state, action) => ({
+      ...state,
+      userName: action.payload,
+      // state.userName = action.payload;
+    }),
 
-    logout(state) {
-      state.token = null;
-      state.userName = '';
-      state.loggedIn = false;
-    },
+    logOut: (state) => ({
+      ...state,
+      token: null,
+      username: '',
+    }),
   },
 });
 
@@ -32,7 +37,7 @@ export const { setToken, setUserName, logout } = authSlice.actions;
 
 export const selectToken = (state) => state.auth.token;
 export const selectUserName = (state) => state.auth.userName;
-export const selectLoggedIn = (state) => state.auth.loggedIn;
+export const selectLoggedIn = (state) => !!state.auth.token;
 
 export default authSlice.reducer;
 
