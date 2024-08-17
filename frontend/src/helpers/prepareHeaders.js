fee2451e195a34487ef2c320b0cd7db90b81c926
Marina Garipova <naumarina@yandex.ru>
2024-08-17 16:45:10 +0800

    refactor: commented code removed

--- frontend/src/helpers/prepareHeaders.js
+++ frontend/src/helpers/prepareHeaders.js
@@ -1,20 +1,9 @@
 const prepareHeaders = (headers) => {
   const token = localStorage.getItem('token');
-  // console.log('Token from localStorage:', token);
-  // console.log('Token from localStorage:', JSON.stringify(token, null, 2));
   if (token) {
     headers.set('Authorization', `Bearer ${token}`);
-    // console.log('Headers with token:', JSON.stringify(headers, null, 2));
-    // headers.forEach((value, key) => {
-    // console.log(`${key}: ${value}`);
-    // });
   }
   return headers;
 };
 
 export default prepareHeaders;
-
-// (headers) => {
-//   headers.set('Authorization', `Bearer ${localStorage.getItem('token')}`);
-//   console.log('localStorage', localStorage.getItem('token'));
-//   return headers;
