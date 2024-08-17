e8788ecc549d64bf2bcd8fc665c1f2b480080b70
Marina Garipova <naumarina@yandex.ru>
2024-08-17 16:45:43 +0800

    refactor: commented code removed

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -5,9 +5,6 @@ import {
 
 const useAuth = () => {
   const dispatch = useDispatch();
-  // const token = useSelector(selectToken);
-  // const userName = useSelector(selectUserName);
-  // const loggedIn = useSelector(selectLoggedIn);
 
   const logIn = (tokenUser, username) => {
     localStorage.setItem('token', tokenUser);
@@ -28,19 +25,3 @@ const useAuth = () => {
 };
 
 export default useAuth;
-
-// import { useContext } from 'react';
-// import AuthContext from '../context/AuthContext';
-
-// // const useAuth = () => useContext(AuthContext);
-
-// const useAuth = () => {
-//   const context = useContext(AuthContext);
-//   if (!context) {
-//     throw new Error('useAuth must be used within an AuthProvider');
-//   }
-//   // console.log('context', context);
-//   return context;
-// };
-
-// export default useAuth;
