baf2bb0525ecd9dfeda71cb69e2c866254cc865b
Marina Garipova <naumarina@yandex.ru>
2024-08-21 11:32:20 +0800

    refactor: chenged path

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import imageNotFound from '../images/errorPage.png';
+import imageNotFound from '../assets/images/errorPage.png';
 
 const NotFound = () => {
   const { t } = useTranslation();
