f2e058b8a398fe6f65de7a61e135ace58b5a91bf
Marina Garipova <naumarina@yandex.ru>
2024-08-21 19:56:55 +0800

    removed

--- frontend/src/helpers/leoDictionary.js
+++
@@ -1,9 +0,0 @@
-import filter from 'leo-profanity';
-import badWords from '../locales/customBadWords.js';
-
-const enWords = filter.getDictionary('en');
-const ruWords = filter.getDictionary('ru');
-
-filter.add(enWords);
-filter.add(ruWords);
-filter.add(badWords);
